db09e3ede9eb229255a88af41ce79846a1390c72
Vladimir Orlov <satancrew.true@gmail.com>
2023-06-26 18:12:09 +0300

    feat: add some tests for 04 task

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,37 +1,44 @@
 // Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import { BankAccount, InsufficientFundsError, getBankAccount } from '.';
 
 describe('BankAccount', () => {
   test('should create account with initial balance', () => {
-    // Write your test here
+    const initialBalance = 50000;
+    const newAccount = getBankAccount(initialBalance);
+    expect(newAccount).toBeInstanceOf(BankAccount);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    const initialBalance = 500;
+    const transferAmount = 800;
+    const newAccount = getBankAccount(initialBalance);
+    expect(() => newAccount.withdraw(transferAmount)).toThrow(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw TransferFailedError error when transferring more than balance', () => {
-    // Write your test here
+    // Write your code here
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    // Write your code here
   });
 
-  test('should deposit money', () => {
-    // Write your test here
+  test('should deposit money', async () => {
+    // Write your code here
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    // Write your code here
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    // Write your code here
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    // Write your code here
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
